72ed28c902ad267fd26a6ba4f3d62b40cb1aeacc
Cedric Beust <cedric@beust.com>
2015-11-10 15:13:45 -0800

    Update

--- example/src/main/java/com/beust/apt/example/Example.java
+++ example/src/main/java/com/beust/apt/example/Example.java
@@ -2,14 +2,25 @@ package com.beust.apt.example;
 
 import com.beust.apt.processor.Version;
 
+import java.io.File;
+
 @Version("1.2")
 class Example {
     public Example() {
         System.out.println("Instantiating Example");
     }
 
-    public static void main(String[] argv) {
+    public static void main(String[] argv) throws Exception {
+        String[] args = new String[] {
+//                "-proc:only",
+                "-classpath", "/Users/beust/home/java/java-apt-example/processor/kobaltBuild/libs/processor-0.1.jar",
+                "-processor", "com.beust.apt.processor.MainProcessor",
+                "/Users/beust/java/java-apt-example/example/src/main/java/com/beust/apt/example/Example.java"
+        };
+        com.sun.tools.javac.Main.main(args);
+
+        File file = new File(".");
+        System.out.println("Instantiating Example()");
         new Example();
     }
-
 }
--- processor/src/main/java/com/beust/apt/processor/MainProcessor.java
+++ processor/src/main/java/com/beust/apt/processor/MainProcessor.java
@@ -2,11 +2,16 @@ package com.beust.apt.processor;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
 import java.util.HashSet;
 import java.util.Set;
 
 public class MainProcessor extends AbstractProcessor {
+    public MainProcessor() {
+        System.out.println("Instantiating MainProcessor");
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         System.out.println("Processing annotations $annotations");
@@ -17,5 +22,10 @@ public class MainProcessor extends AbstractProcessor {
         Set<String> result = new HashSet<>();
         result.add("Version");
         return result;
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.RELEASE_7;
     }
 }
