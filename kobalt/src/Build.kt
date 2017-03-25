2f59d4c47f7a6833f2571c079d23e3090f02e8b7
Cedric Beust <cedric@beust.com>
2017-03-25 14:29:08 -0700

    Fix build.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -16,10 +16,7 @@ val processor = project {
     directory = "processor"
 
     assemble {
-        jar {
-            include(from("service"), to(""), glob("**/*"))
-            include(from("kobaltBuild/classes"), to(""), glob("**/*"))
-        }
+        mavenJars {}
     }
 
     bintray {
@@ -27,20 +24,12 @@ val processor = project {
     }
 }
 
-// We need to depend on `processor` so we get built after but we need to depend
-// on processor.jar, which contains the additional text file defining the annotation processor
 val processorExample = project(processor) {
     name = "example"
     group = "com.beust"
     artifactId = name
     version = VERSION
     directory = "example"
-
-    val processorJar = file("processor/kobaltBuild/libs/version-processor-$VERSION.jar")
-
-    dependencies {
-        compile(processorJar)
-    }
 
     assemble {
         jar {
